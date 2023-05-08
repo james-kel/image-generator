f7446375c26ae15b941dd2da79c78b0d7f35d084
james_k <jamesk91@live.co.uk>
2023-05-08 20:45:00 +0100

    Created selectRandomImage and displayImage function expressions and a click event

--- js/script.js
+++ js/script.js
@@ -6,7 +6,27 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    //console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imagesAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imagesAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+    getImage();
+});
